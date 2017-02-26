f11d0c2fe2f4b48891e7ea2594e359efd6e95ce3
chrisguttandin <chrisguttandin@media-codings.com>
2017-02-26 05:00:22 +0100

    apply linting rules

--- src/helper/write-variable-length-quantity.ts
+++ src/helper/write-variable-length-quantity.ts
@@ -5,7 +5,9 @@ export const writeVariableLengthQuantity = (value) => {
 
     const { arrayBuffer, dataView } = createArrayBufferWithDataView(numberOfBytes);
 
-    for (let i = 0, length = numberOfBytes - 1; i < length; i += 1) {
+    const length = numberOfBytes - 1;
+
+    for (let i = 0; i < length; i += 1) {
         dataView.setUint8(i, (value >> 7) | 0x80); // tslint:disable-line:no-bitwise
 
         value &= 0x7F; // tslint:disable-line:no-bitwise
